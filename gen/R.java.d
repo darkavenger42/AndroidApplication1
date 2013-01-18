6c201ec493157fed3bc3b6c28c09b170e7b9993b
Dark Avenger <darkavenger42@hotmail.com>
2013-01-18 13:30:29 -0300

    teste testoso

---
+++ gen/R.java.d
@@ -0,0 +1,4 @@
+C:\Users\Danilo\Dropbox\netbeans projects\AndroidApplication1\gen\br\teste\com\R.java \
+ : C:\Users\Danilo\Dropbox\netbeans projects\AndroidApplication1\res\layout\main.xml \
+C:\Users\Danilo\Dropbox\netbeans projects\AndroidApplication1\res\values\strings.xml \
+C:\Users\Danilo\Dropbox\netbeans projects\AndroidApplication1\bin\AndroidManifest.xml \
